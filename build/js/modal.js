5d13d697af8f0a43c06063e8e4d87ca3edc68964
Gavrilovph <vladimir.gavrilov.ph@yandex.ru>
2023-08-28 16:23:04 +0400

    Node modules на удаление

--- build/js/modal.js
+++ build/js/modal.js
@@ -1,12 +1,11 @@
-let openModalbtn = document.querySelector('.modal__btn');
-let openModalWindow = document.querySelector('.modal');
-let hideModalBtn = document.querySelector('.modal__close');
+let openModalbtn = document.querySelector(".modal__btn");
+let openModalWindow = document.querySelector(".modal");
+let hideModalBtn = document.querySelector(".modal__close");
 
-openModalbtn.addEventListener('click', () => {
-    openModalWindow.classList.add('open')
-})
+openModalbtn.addEventListener("click", () => {
+  openModalWindow.classList.add("open");
+});
 
-hideModalBtn.addEventListener('click', () => {
-    openModalWindow.classList.remove('open')
-})
-
+hideModalBtn.addEventListener("click", () => {
+  openModalWindow.classList.remove("open");
+});
